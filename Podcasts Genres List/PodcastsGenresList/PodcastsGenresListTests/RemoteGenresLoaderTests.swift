9b172caf8aac35d9bb628b7a64e6ee0c4c5eb4d1
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-08-31 16:36:11 +0300

    Request data from URL on load

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
@@ -9,18 +9,34 @@ import XCTest
 
 class RemoteGenresLoader {
     
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient {
     var requestedURL: URL?
+    
+    static let shared = HTTPClient()
+    
+    private init() {}
 }
 
 class RemoteGenresLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteGenresLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteGenresLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }

a804c52f967bcda1de753b3f50152a7643f98638
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-08-31 17:00:14 +0300

    Give a URL instead of hardcoding URL

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
@@ -10,13 +10,15 @@ import XCTest
 class RemoteGenresLoader {
     
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "http://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -36,18 +38,20 @@ class HTTPClientSpy: HTTPClient {
 class RemoteGenresLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteGenresLoader(client: client)
+        _ = RemoteGenresLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "http://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteGenresLoader(client: client)
+        let sut = RemoteGenresLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }

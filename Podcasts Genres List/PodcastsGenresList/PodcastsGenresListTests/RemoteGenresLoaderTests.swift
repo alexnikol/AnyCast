d3d2d83ee6d4a4aac11835b3fb871ffb756f2d8a
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-08-31 17:09:00 +0300

    extract sut to helper method, moved `HTTPClientSpy ` inside tests class scope

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class RemoteGenresLoader {
-    
     let client: HTTPClient
     let url: URL
     
@@ -26,32 +25,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteGenresLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "http://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteGenresLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteGenresLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (loader: RemoteGenresLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteGenresLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }

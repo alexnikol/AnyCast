517dde95d9abf93fed47ce836def15a989d57ae7
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-01 10:34:03 +0300

    Guarantee we only receive 1 error

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
@@ -39,12 +39,10 @@ class RemoteGenresLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         client.error = NSError(domain: "Test", code: 0)
         
-        var capturedError: RemoteGenresLoader.Error?
-        sut.load { error in
-            capturedError = error
-        }
+        var capturedError: [RemoteGenresLoader.Error] = []
+        sut.load { capturedError.append($0) }
         
-        XCTAssertEqual(capturedError, .connectivity)
+        XCTAssertEqual(capturedError, [.connectivity])
     }
     
     // MARK: - Helpers

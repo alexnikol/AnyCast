6972f64170bd7e04bca5f874ea06fb9876b48fcb
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-01 10:31:54 +0300

    Delivers connectivity error on client error

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/RemoteGenresLoaderTests.swift
@@ -35,6 +35,18 @@ class RemoteGenresLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteGenresLoader.Error?
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (loader: RemoteGenresLoader, client: HTTPClientSpy) {
@@ -45,8 +57,12 @@ class RemoteGenresLoaderTests: XCTestCase {
     
     private final class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }

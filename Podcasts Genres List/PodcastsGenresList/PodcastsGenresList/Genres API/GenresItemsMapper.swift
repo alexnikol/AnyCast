e79c2b8dff5a7bd0feead5395c26ad050446420a
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-08 14:48:26 +0300

    Extract mapping logic to helper static method

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres API/GenresItemsMapper.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres API/GenresItemsMapper.swift
@@ -2,32 +2,24 @@
 
 import Foundation
 
+struct RemoteGenre: Decodable {
+    let id: Int
+    let name: String
+}
+
 final class GenresItemsMapper {
     
     private struct Root: Decodable {
-        let genres: [Item]
-        
-        var list: [Genre] {
-            return genres.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        let id: Int
-        let name: String
-        
-        var item: Genre {
-            return .init(id: id, name: name)
-        }
+        let genres: [RemoteGenre]
     }
     
     private static var OK_200: Int { return 200 }
         
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteGenresLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteGenre] {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteGenresLoader.Error.invalidData)
+            throw RemoteGenresLoader.Error.invalidData
         }
         
-        return .success(root.list)
+        return root.genres
     }
 }

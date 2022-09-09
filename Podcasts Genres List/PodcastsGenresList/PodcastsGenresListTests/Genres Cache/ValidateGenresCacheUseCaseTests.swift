f13466df96d0c858f931b2e384dbe139dedaccba
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-09 14:37:42 +0300

    `LocalGenresLoader` does not message store upon creation (before validation)

---
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/Genres Cache/ValidateGenresCacheUseCaseTests.swift
@@ -0,0 +1,27 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import XCTest
+import PodcastsGenresList
+
+class ValidateGenresCacheUseCaseTests: XCTestCase {
+    
+    func test_init() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: LocalGenresLoader, store: GenresStoreSpy) {
+        let store = GenresStoreSpy()
+        let sut = LocalGenresLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}

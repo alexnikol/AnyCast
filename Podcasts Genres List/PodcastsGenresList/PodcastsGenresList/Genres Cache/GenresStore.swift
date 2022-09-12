00119c11a97fafda8930fdcbe025a085744e76f2
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-12 17:16:48 +0300

    Added comments that all completion handlers of any `GenresStore` implementation can be invoked in any thread

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres Cache/GenresStore.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres Cache/GenresStore.swift
@@ -13,7 +13,15 @@ public protocol GenresStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCacheGenresResult) -> Void
     
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func deleteCacheGenres(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func insert(_ genres: [LocalGenre], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to appropriate threads, if needed
     func retrieve(completion: @escaping RetrievalCompletion)
 }

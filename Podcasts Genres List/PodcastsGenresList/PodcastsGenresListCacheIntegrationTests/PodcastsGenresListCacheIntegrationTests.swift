5bb1d5fe5567a8c567a2b90486811df2464d9785
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-13 16:13:03 +0300

    `LocalGenresLoader` Delivers no items on empty cache

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListCacheIntegrationTests/PodcastsGenresListCacheIntegrationTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListCacheIntegrationTests/PodcastsGenresListCacheIntegrationTests.swift
@@ -1,8 +1,49 @@
 // Copyright © 2022 Almost Engineer. All rights reserved.
 
 import XCTest
+import PodcastsGenresList
 
 class PodcastsGenresListCacheIntegrationTests: XCTestCase {
     
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case .success(let genres):
+                XCTAssertEqual(genres, [], "Expected empty genres")
+
+            case .failure(let error):
+                XCTFail("Expected successful genres result, got \(error) instead")
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> LocalGenresLoader {
+        let storeBundle = Bundle(for: CoreDataGenresStore.self)
+        let storeURL = specificTestStoreURL()
+        let store = try! CoreDataGenresStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalGenresLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func specificTestStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }

e0b48cc6d1b1bbf1f440b0f2c3745432d233848c
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-13 14:18:52 +0300

    Extract reusable CoreData helpers into a separate file

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres Cache/Infrastructure/CoreData/CoreDataGenresStore.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres Cache/Infrastructure/CoreData/CoreDataGenresStore.swift
@@ -2,7 +2,7 @@
 
 import CoreData
 
-public class CoreDataGenresStore: GenresStore {
+public final class CoreDataGenresStore: GenresStore {
     
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
@@ -66,36 +66,6 @@ public class CoreDataGenresStore: GenresStore {
         context.perform { action(context) }
     }
 }
-
-private extension NSPersistentContainer {
-     enum LoadingError: Swift.Error {
-         case modelNotFound
-         case failedToLoadPersistentStores(Swift.Error)
-     }
-
-     static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-             throw LoadingError.modelNotFound
-         }
-
-         let description = NSPersistentStoreDescription(url: url)
-         let container = NSPersistentContainer(name: name, managedObjectModel: model)
-         container.persistentStoreDescriptions = [description]
-         var loadError: Swift.Error?
-         container.loadPersistentStores { loadError = $1 }
-         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
-         return container
-     }
- }
-
- private extension NSManagedObjectModel {
-     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-         return bundle
-             .url(forResource: name, withExtension: "momd")
-             .flatMap { NSManagedObjectModel(contentsOf: $0) }
-     }
- }
 
 @objc(ManagedGenresStoreCache)
 private class ManagedGenresStoreCache: NSManagedObject {

3dca5ffbc0523c628408df9270c032e426e577ba
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-16 14:05:27 +0300

    `GenresPresenter` shows loading view on didStartLoadingGenres

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/Genres Presentation/GenresPresenterTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/Genres Presentation/GenresPresenterTests.swift
@@ -2,8 +2,23 @@
 
 import XCTest
 
+struct GenresLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol GenresLoadingView {
+    func display(_ viewModel: GenresLoadingViewModel)
+}
+
 class GenresPresenter {
-    init(view: Any) {
+    let loadingView: GenresLoadingView
+    
+    init(loadingView: GenresLoadingView) {
+        self.loadingView = loadingView
+    }
+    
+    func didStartLoadingGenres() {
+        loadingView.display(.init(isLoading: true))
     }
 }
 
@@ -15,16 +30,32 @@ class GenresPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingGenres_displaysLoadingMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingGenres()
+        
+        XCTAssertEqual(view.messages, [.showLoadingView])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: GenresPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = GenresPresenter(view: view)
+        let sut = GenresPresenter(loadingView: view)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages: [Any] = []
+    private class ViewSpy: GenresLoadingView {
+        enum Message: Equatable {
+            case showLoadingView
+        }
+        
+        private(set) var messages: [Message] = []
+        
+        func display(_ viewModel: GenresLoadingViewModel) {
+            messages.append(.showLoadingView)
+        }
     }
 }

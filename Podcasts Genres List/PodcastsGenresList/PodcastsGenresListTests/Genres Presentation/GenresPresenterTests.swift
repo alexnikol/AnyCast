2d1e0dff5b7e171ac4fd6c2cf898549ada13423b
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-16 13:03:23 +0300

    Extract system under test creation into a test factory method

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/Genres Presentation/GenresPresenterTests.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListTests/Genres Presentation/GenresPresenterTests.swift
@@ -10,14 +10,19 @@ class GenresPresenter {
 class GenresPresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = GenresPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: GenresPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = GenresPresenter(view: view)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
     
     private class ViewSpy {
         let messages: [Any] = []

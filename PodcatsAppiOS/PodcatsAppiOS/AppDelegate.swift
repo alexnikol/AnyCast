fcc611b83c46ed09becbd20f66950ca546e5ff8d
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-16 15:43:44 +0300

    Composed application with PodcastsGenres modules

--- PodcatsAppiOS/PodcatsAppiOS/AppDelegate.swift
+++ PodcatsAppiOS/PodcatsAppiOS/AppDelegate.swift
@@ -6,31 +6,26 @@
 //
 
 import UIKit
+import PodcastsGenresList
+import PodcastsGenresListiOS
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let session = URLSession(configuration: .ephemeral)
+        let httpClient = URLSessionHTTPClient(session: session)
+        let baseURL = URL(string: "https://listen-api-test.listennotes.com")!
+        let genresRequestPath = baseURL.appendingPathComponent("api/v2/genres")
+        let genresLoader = RemoteGenresLoader(url: genresRequestPath, client: httpClient)
+        let genresController = GenresUIComposer.genresComposedWith(loader: genresLoader)
+        let nav = UINavigationController(rootViewController: genresController)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-

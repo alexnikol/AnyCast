89bba18025d33004f3f2f8e4f3d2730afca0011c
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-09-16 15:26:44 +0300

    Create `PodcatsAppiOS` workspace combining `PodcatsAppiOS` and `PodcastsGenresList`

---
+++ PodcatsAppiOS/PodcatsAppiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcatsAppiOS
+//
+//  Created by Alexander Nikolaychuk on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

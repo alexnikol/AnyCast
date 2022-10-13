55036f98d5d5bab86e87aa327f9a299724f6d00b
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-13 11:23:03 +0300

    Integrated `GenresActiveColorProvider` to genres view adapter

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListiOS/Genres UI/Composers/GenresUIComposer.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListiOS/Genres UI/Composers/GenresUIComposer.swift
@@ -14,9 +14,33 @@ public final class GenresUIComposer {
         genresController.title = GenresPresenter.title
         
         presentationAdapter.presenter = GenresPresenter(
-            genresView: GenresViewAdapter(controller: genresController),
+            genresView: GenresViewAdapter(
+                controller: genresController,
+                genresColorProvider: makeGenresColorProvider()
+            ),
             loadingView: WeakRefVirtualProxy(refreshController)
         )
         return genresController
+    }
+    
+    private static func makeGenresColorProvider() -> GenresActiveColorProvider {
+        let genresActiveColorProvider = GenresActiveColorProvider()
+        do {
+            try genresActiveColorProvider.setColors(
+                ["#e6194b", "#3cb44b", "#ffe119", "#4363d8",
+                 "#f58231", "#911eb4", "#46f0f0", "#f032e6",
+                 "#bcf60c", "#fabebe", "#008080", "#e6beff",
+                 "#9a6324", "#fffac8", "#800000", "#aaffc3",
+                 "#808000", "#ffd8b1", "#000075", "#808080",
+                 "#E574BC", "#EF94D5", "#F9B4ED", "#EABAF6",
+                 "#DABFFF", "#C4C7FF", "#ADCFFF", "#96D7FF",
+                 "#7FDEFF", "#FFC8DD", "#FF70A5", "#BDE0FE",
+                 "#99CEFF", "#FFC8DD", "#FFAFCC", "#BDE0FE",
+                 "#A2D2FF", "#EA84C9"]
+            )
+        } catch {
+            fatalError("Unexpected colors set with error \(error)")
+        }
+        return genresActiveColorProvider
     }
 }
--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresListiOS/Genres UI/Composers/GenresViewAdapter.swift
+++ Podcasts Genres List/PodcastsGenresList/PodcastsGenresListiOS/Genres UI/Composers/GenresViewAdapter.swift
@@ -5,15 +5,22 @@ import PodcastsGenresList
 
 final class GenresViewAdapter: GenresView {
     private weak var controller: GenresListViewController?
+    private let genresColorProvider: GenresActiveColorProvider
     
-    init(controller: GenresListViewController) {
+    init(controller: GenresListViewController, genresColorProvider: GenresActiveColorProvider) {
         self.controller = controller
+        self.genresColorProvider = genresColorProvider
     }
 
     func display(_ viewModel: GenresViewModel) {
         controller?.collectionModel = viewModel.genres.enumerated().map { (index, model) in
-            let cellModel = GenreCellViewModel(name: model.name, color: .green)
+            let cellModel = GenreCellViewModel(name: model.name, color: associatedColorByIndex(index))
             return GenreCellController(model: cellModel)
         }
+    }
+    
+    private func associatedColorByIndex(_ index: Int) -> UIColor {
+        let defaultColor = UIColor.white
+        return (try? genresColorProvider.getColor(by: index)) ?? defaultColor
     }
 }

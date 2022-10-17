7a84fcc973f56e2a6e51cad530a512ca0f9df1f2
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-17 13:06:47 +0300

    Extract mapping logic into separate method and extensions helpers

--- Podcasts Genres List/PodcastsGenresList/BestPodcastsListTests/LoadBestPodcastsFromRemoteUseCaseTests.swift
+++ Podcasts Genres List/PodcastsGenresList/BestPodcastsListTests/LoadBestPodcastsFromRemoteUseCaseTests.swift
@@ -59,23 +59,31 @@ class RemoteBestPodcastsLoader {
                 completion(.failure(Error.connectivity))
                 
             case let .success((data, response)):
-                do {
-                    let remotePodcastsList = try BestPodastsItemsMapper.map(data, from: response)
-                    let podcasts = remotePodcastsList.podcasts.map {
-                        Podcast(id: $0.id, title: $0.title, image: $0.image)
-                    }
-                    
-                    let bestPodcastsList = BestPodcastsList(
-                        genreId: remotePodcastsList.genreId,
-                        genreName: remotePodcastsList.genreName,
-                        podcasts: podcasts
-                    )
-                    
-                    completion(.success(bestPodcastsList))
-                } catch {
-                    completion(.failure(error))
-                }
+                completion(Self.map(data: data, response: response))
             }
+        }
+    }
+    
+    private static func map(data: Data, response: HTTPURLResponse) -> Result {
+        do {
+            let remoteList = try BestPodastsItemsMapper.map(data, from: response)
+            return .success(remoteList.toModel())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
+extension RemoteBestPodcastsList {
+    func toModel() -> BestPodcastsList {
+        return BestPodcastsList(genreId: genreId, genreName: genreName, podcasts: podcasts.toModels())
+    }
+}
+
+extension Array where Element == RemotePodcast {
+    func toModels() -> [Podcast] {
+        return map {
+            Podcast(id: $0.id, title: $0.title, image: $0.image)
         }
     }
 }

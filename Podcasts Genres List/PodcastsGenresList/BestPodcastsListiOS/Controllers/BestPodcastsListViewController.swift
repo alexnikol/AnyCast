34617564a26b17ebe4e06b4ee17c386b52619ec3
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-10-31 17:01:07 +0200

    loadPodcastsActions requests list of Podcast by genre on load and on each refreshControler value changed

--- Podcasts Genres List/PodcastsGenresList/BestPodcastsListiOS/Controllers/BestPodcastsListViewController.swift
+++ Podcasts Genres List/PodcastsGenresList/BestPodcastsListiOS/Controllers/BestPodcastsListViewController.swift
@@ -1,8 +1,54 @@
 // Copyright © 2022 Almost Engineer. All rights reserved.
 
 import UIKit
+import BestPodcastsList
+
+class PodcastCellController {}
 
 public final class BestPodcastsListViewController: UITableViewController {
+    private var loader: BestPodcastsLoader
+    private var genreID: Int
+    private var cells: [PodcastCellController] = []
     
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        self.refreshControl = refreshControl
+        refresh()
+    }
     
+    @objc
+    private func refresh() {
+        loader.load(by: genreID, completion: { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .success(data):
+                self.cells = data.podcasts.map { _ in PodcastCellController() }
+                self.tableView.reloadData()
+            case .failure:
+                break
+            }
+        })
+    }
+    
+    public init(genreID: Int, loader: BestPodcastsLoader) {
+        self.genreID = genreID
+        self.loader = loader
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
--- Podcasts Genres List/PodcastsGenresList/BestPodcastsListiOSTests/BestPodcastsListiOSTests.swift
+++ Podcasts Genres List/PodcastsGenresList/BestPodcastsListiOSTests/BestPodcastsListiOSTests.swift
@@ -6,14 +6,29 @@ import BestPodcastsListiOS
 
 class BestPodcastsListiOSTests: XCTestCase {
     
+    func test_loadPodcastsActions_requestPodcastsByGenreFromLoader() {
+        let (sut, loader) = makeSUT()
+        XCTAssertEqual(loader.loadCallCount, 0, "Expected no loading requests before view is loaded")
+        
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadCallCount, 1, "Expected a loading request once view is loaded")
+        
+        sut.simulateUserInitiatedPodcastsListReload()
+        XCTAssertEqual(loader.loadCallCount, 2, "Expected another loading request once user initiates a load")
+        
+        sut.simulateUserInitiatedPodcastsListReload()
+        XCTAssertEqual(loader.loadCallCount, 3, "Expected a third loading request once user initiates a load")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(
+        genreID: Int = 1,
         file: StaticString = #file,
         line: UInt = #line
     ) -> (sut: BestPodcastsListViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
-        let sut = BestPodcastsListViewController()
+        let sut = BestPodcastsListViewController(genreID: genreID, loader: loader)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         return (sut, loader)
@@ -21,6 +36,29 @@ class BestPodcastsListiOSTests: XCTestCase {
     
     private class LoaderSpy: BestPodcastsLoader {
         
-        func load(by genreID: Int, completion: @escaping (BestPodcastsLoader.Result) -> Void) {}
+        private var completions: [(BestPodcastsLoader.Result) -> Void] = []
+        
+        var loadCallCount: Int {
+            return completions.count
+        }
+        
+        func load(by genreID: Int, completion: @escaping (BestPodcastsLoader.Result) -> Void) {
+            completions.append(completion)
+        }
+    }
+}
+
+private extension BestPodcastsListViewController {
+    func simulateUserInitiatedPodcastsListReload() {
+        tableView.refreshControl?.simulatePullToRefresh()
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        self.allTargets.forEach { target in
+                actions(forTarget: target, forControlEvent: .valueChanged)?
+                .forEach { (target as NSObject).perform(Selector($0)) }
+        }
     }
 }

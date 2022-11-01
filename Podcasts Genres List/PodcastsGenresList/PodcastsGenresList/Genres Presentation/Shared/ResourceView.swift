a7ea3094363219a47755ee041c990e6b775a1263
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-01 10:11:50 +0200

    Remove redundant shared presentation logic

--- Podcasts Genres List/PodcastsGenresList/PodcastsGenresList/Genres Presentation/Shared/ResourceView.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2022 Almost Engineer. All rights reserved.
-
-import Foundation
-
-public protocol ResourceView {
-    associatedtype ResourceViewModel
-    
-    func display(_ viewModel: ResourceViewModel)
-}

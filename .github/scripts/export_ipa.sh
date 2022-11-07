a48e68af8f2f5011711064eac203ee45fa5215dd
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-07 13:17:46 +0200

    remove unneeded scripts

--- .github/scripts/export_ipa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -archivePath $PWD/build/Podcats.xcarchive \
-            -exportOptionsPlist .github/secrets/exportOptions.plist \
-            -exportPath $PWD/build \
-            -allowProvisioningUpdates \
-            -exportArchive | xcpretty

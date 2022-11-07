99a5027fe4b0ae1da2e46993e366f5c7f013ee26
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-07 18:01:17 +0200

    slathered fix

--- PodcastsComponents/PodcastsComponents/Networking/HTTPClientTests/HTTPClientTests.swift
+++ PodcastsComponents/PodcastsComponents/Networking/HTTPClientTests/HTTPClientTests.swift
@@ -1,7 +1,11 @@
 // Copyright © 2022 Almost Engineer. All rights reserved.
 
 import XCTest
+import HTTPClient
 
 class HTTPClientTests: XCTestCase {
-    func test_load() {}
+    
+    func test_setup() {
+        XCTAssertEqual("qwe", "qwe")
+    }
 }

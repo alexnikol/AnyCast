3a8f825a7f9438b2e010fc9ea18d9eefa41bae87
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-09 17:42:20 +0200

    Changed accent color

--- PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/PodcastDetails/Views/EpisodeCell/EpisodeCell.swift
+++ PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/PodcastDetails/Views/EpisodeCell/EpisodeCell.swift
@@ -3,12 +3,15 @@
 import UIKit
 
 public final class EpisodeCell: UITableViewCell, NibReusable {
+    @IBOutlet public private(set) weak var dateLabel: UILabel!
     @IBOutlet public private(set) weak var titleLabel: UILabel!
     @IBOutlet public private(set) weak var descriptionLabel: UILabel!
     @IBOutlet public private(set) weak var audioLengthLabel: UILabel!
+    @IBOutlet private(set) weak var cellContainer: UIView!
         
     override public func awakeFromNib() {
         super.awakeFromNib()
+        cellContainer.layer.cornerRadius = 4.0
     }
 
     override public func setSelected(_ selected: Bool, animated: Bool) {}

5f52fee763729d84f87cd307f4f79f684611e4ce
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-09 13:06:42 +0200

    Added  public init for tests needs

--- PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModule/BestPodcasts/BestPodcasts Presentation/PodcastImageViewModel.swift
+++ PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModule/BestPodcasts/BestPodcasts Presentation/PodcastImageViewModel.swift
@@ -10,4 +10,20 @@ public struct PodcastImageViewModel {
     public let typeStaticLabel: String
     public let typeValueLabel: String
     public let image: URL
+    
+    public init(title: String,
+                publisher: String,
+                languageStaticLabel: String,
+                languageValueLabel: String,
+                typeStaticLabel: String,
+                typeValueLabel: String,
+                image: URL) {
+        self.title = title
+        self.publisher = publisher
+        self.languageStaticLabel = languageStaticLabel
+        self.languageValueLabel = languageValueLabel
+        self.typeStaticLabel = typeStaticLabel
+        self.typeValueLabel = typeValueLabel
+        self.image = image
+    }
 }

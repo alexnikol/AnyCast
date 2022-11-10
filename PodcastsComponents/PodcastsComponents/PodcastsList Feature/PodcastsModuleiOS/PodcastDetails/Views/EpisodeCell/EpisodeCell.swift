4352638d31c73f3a67c9c1aacb4e7532857ca5bc
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-10 12:49:11 +0200

    Added Hightlight state for Episode cell

--- PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/PodcastDetails/Views/EpisodeCell/EpisodeCell.swift
+++ PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/PodcastDetails/Views/EpisodeCell/EpisodeCell.swift
@@ -3,6 +3,10 @@
 import UIKit
 
 public final class EpisodeCell: UITableViewCell, NibReusable {
+    private enum Defaults {
+        static let defaultBackground: UIColor = .secondarySystemBackground
+        static let highlightedBackground: UIColor = .tertiarySystemBackground
+    }
     @IBOutlet public private(set) weak var publishDateLabel: UILabel!
     @IBOutlet public private(set) weak var titleLabel: UILabel!
     @IBOutlet public private(set) weak var descriptionLabel: UILabel!
@@ -14,5 +18,9 @@ public final class EpisodeCell: UITableViewCell, NibReusable {
         cellContainer.layer.cornerRadius = 4.0
     }
 
-    override public func setSelected(_ selected: Bool, animated: Bool) {}
+    public override func setSelected(_ selected: Bool, animated: Bool) {}
+    
+    public override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        cellContainer.backgroundColor = highlighted ? Defaults.highlightedBackground : Defaults.defaultBackground
+    }
 }

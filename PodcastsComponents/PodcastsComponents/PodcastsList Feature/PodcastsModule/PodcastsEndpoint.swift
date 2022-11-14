3188f8cabdc38670932d17b33b2d7e82a77f4393
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-14 13:55:22 +0200

    Extract duplication to shared method `PodcastsEndpoint`

--- PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModule/PodcastsEndpoint.swift
+++ PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModule/PodcastsEndpoint.swift
@@ -9,19 +9,20 @@ public enum PodcastsEndpoint {
     public func url(baseURL: URL) -> URL {
         switch self {
         case .getPodcastDetails(let podcastID):
-            var urlBuilder = URLComponents()
-            urlBuilder.scheme = baseURL.scheme
-            urlBuilder.host = baseURL.host
-            urlBuilder.path = "/api/v2/podcasts/\(podcastID)"
-            return urlBuilder.url!
+            return build(fromBaseURL: baseURL, path: "/api/v2/podcasts/\(podcastID)").url!
             
         case .getBestPodcasts(let genreID):
-            var urlBuilder = URLComponents()
-            urlBuilder.scheme = baseURL.scheme
-            urlBuilder.host = baseURL.host
-            urlBuilder.path = "/api/v2/best_podcasts"
+            var urlBuilder = build(fromBaseURL: baseURL, path: "/api/v2/best_podcasts")
             urlBuilder.queryItems = [URLQueryItem(name: "genre_id", value: String(genreID))]
             return urlBuilder.url!
         }
+    }
+    
+    private func build(fromBaseURL url: URL, path: String) -> URLComponents {
+        var urlBuilder = URLComponents()
+        urlBuilder.scheme = url.scheme
+        urlBuilder.host = url.host
+        urlBuilder.path = path
+        return urlBuilder
     }
 }

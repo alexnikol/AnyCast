3ce51a263fce7b4a4f982061a4e9aa64a0fe9bdb
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-17 16:28:20 +0200

    Fixed layout colors for Podcast Header reusable view

--- PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/BestPodcasts/Views/PodcastHeaderReusableView/PodcastHeaderReusableView.swift
+++ PodcastsComponents/PodcastsComponents/PodcastsList Feature/PodcastsModuleiOS/BestPodcasts/Views/PodcastHeaderReusableView/PodcastHeaderReusableView.swift
@@ -7,6 +7,7 @@ public final class PodcastHeaderReusableView: UITableViewHeaderFooterView, Reusa
         
     @IBOutlet public private(set) weak var titleLabel: UILabel!
     @IBOutlet public private(set) weak var authorLabel: UILabel!
+    @IBOutlet private weak var mainContainer: UIView!
     @IBOutlet public private(set) weak var imageMainContainer: UIView!
     @IBOutlet public private(set) weak var imageInnerContainer: UIView!
     @IBOutlet public private(set) weak var imageView: UIImageView!
@@ -17,6 +18,7 @@ public final class PodcastHeaderReusableView: UITableViewHeaderFooterView, Reusa
     
     override public func awakeFromNib() {
         super.awakeFromNib()
+        mainContainer.layer.cornerRadius = 4.0
         imageInnerContainer.layer.cornerRadius = 4.0
         imageMainContainer.layer.cornerRadius = 4.0
         imageMainContainer.layer.shadowColor = UIColor.tintColor.cgColor

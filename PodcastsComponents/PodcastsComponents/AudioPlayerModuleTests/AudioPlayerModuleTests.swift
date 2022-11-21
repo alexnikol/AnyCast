14c37cae8d0546ebc55ba4995569b812f1d327c1
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-21 16:51:25 +0200

    Move creation logic of SUT into factory method

--- PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/AudioPlayerModuleTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/AudioPlayerModuleTests.swift
@@ -4,7 +4,11 @@ import XCTest
 import PodcastsModule
 
 class PlayingEpisodeService {
+    private let store: PlayingEpisodeStore
     
+    init(store: PlayingEpisodeStore) {
+        self.store = store
+    }
 }
 
 protocol PlayingEpisodeStore {}
@@ -12,10 +16,19 @@ protocol PlayingEpisodeStore {}
 class PlayingEpisodeServiceTests: XCTestCase {
     
     func test_init() {
-        let sut = PlayingEpisodeService()
-        let store = PlayingEpisodeStoreSpy()
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.messages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: PlayingEpisodeService, store: PlayingEpisodeStoreSpy) {
+        let store = PlayingEpisodeStoreSpy()
+        let sut = PlayingEpisodeService(store: store)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
     }
 }
 

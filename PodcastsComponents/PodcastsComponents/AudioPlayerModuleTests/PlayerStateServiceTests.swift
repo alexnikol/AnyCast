60c23f17582098d71dfcd2abe1a0f1a7a21251cf
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-22 12:51:43 +0200

    Extract sut creation into factory method

--- PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/PlayerStateServiceTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/PlayerStateServiceTests.swift
@@ -11,32 +11,44 @@ protocol AudioPlayerObserver {
     func receive(_ playerState: CurrentPlayerState)
 }
 
-protocol AudioPlayerStateSubject {
+protocol AudioPlayerStatePublisher {
     func subscribe(observer: AudioPlayerObserver)
+}
+
+class AudioPlayerStateObserver: AudioPlayerObserver {
+    var onReceiveUpdates: (CurrentPlayerState) -> Void
+    
+    init(onReceiveUpdates: @escaping (CurrentPlayerState) -> Void) {
+        self.onReceiveUpdates = onReceiveUpdates
+    }
+    
+    func receive(_ playerState: CurrentPlayerState) {
+        onReceiveUpdates(playerState)
+    }
 }
 
 class PlayerStateServiceTests: XCTestCase {
     
     func test_init_doesNotDeliversValueOnCreationWithoutSubscription() {
-        var receivedData: CurrentPlayerState?
-        _ = AudioPlayerStateObserver(onReceiveUpdates: { state in
-            receivedData = state
-        })
+        var receivedPlayerState: CurrentPlayerState?
         
-        XCTAssertNil(receivedData)
+        _ = makeSUT { state in
+            receivedPlayerState = state
+        }
+        
+        XCTAssertNil(receivedPlayerState)
     }
     
     func test_onReceiveValues_deliversStateAfterSubscription() {
-        let playerSubject = AudioPlayerStateSubjectSpy()
         var receivedPlayerState: CurrentPlayerState?
+        
         let exp = expectation(description: "Wait on receive state")
-        let observer = AudioPlayerStateObserver(onReceiveUpdates: { state in
+        let (sut, publisher) = makeSUT { state in
             receivedPlayerState = state
             exp.fulfill()
-        })
-        
-        playerSubject.subscribe(observer: observer)
-        playerSubject.receiveNewPlayerState(.noPlayingItem)
+        }
+        publisher.subscribe(observer: sut)
+        publisher.receiveNewPlayerState(.noPlayingItem)
         
         wait(for: [exp], timeout: 1.0)
         
@@ -44,16 +56,16 @@ class PlayerStateServiceTests: XCTestCase {
     }
     
     func test_onReceiveValues_deliversPreviousStateAfterSubscription() {
-        let playerSubject = AudioPlayerStateSubjectSpy()
         var receivedPlayerState: CurrentPlayerState?
+        
         let exp = expectation(description: "Wait on receive state")
-        let observer = AudioPlayerStateObserver(onReceiveUpdates: { state in
+        let (sut, publisher) = makeSUT { state in
             receivedPlayerState = state
             exp.fulfill()
-        })
+        }
         
-        playerSubject.receiveNewPlayerState(.noPlayingItem)
-        playerSubject.subscribe(observer: observer)
+        publisher.receiveNewPlayerState(.noPlayingItem)
+        publisher.subscribe(observer: sut)
         
         wait(for: [exp], timeout: 1.0)
         
@@ -62,19 +74,19 @@ class PlayerStateServiceTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private class AudioPlayerStateObserver: AudioPlayerObserver {
-        var onReceiveUpdates: (CurrentPlayerState) -> Void
-        
-        init(onReceiveUpdates: @escaping (CurrentPlayerState) -> Void) {
-            self.onReceiveUpdates = onReceiveUpdates
-        }
-        
-        func receive(_ playerState: CurrentPlayerState) {
-            onReceiveUpdates(playerState)
-        }
+    private func makeSUT(
+        onReceiveUpdates: @escaping (CurrentPlayerState) -> Void,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: AudioPlayerStateObserver, publisher: AudioPlayerStatePublisherSpy) {
+        let publisher = AudioPlayerStatePublisherSpy()
+        let sut = AudioPlayerStateObserver(onReceiveUpdates: onReceiveUpdates)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(publisher, file: file, line: line)
+        return (sut, publisher)
     }
     
-    private class AudioPlayerStateSubjectSpy: AudioPlayerStateSubject {
+    private class AudioPlayerStatePublisherSpy: AudioPlayerStatePublisher {
         
         private var previosState: CurrentPlayerState?
         private var observer: AudioPlayerObserver?

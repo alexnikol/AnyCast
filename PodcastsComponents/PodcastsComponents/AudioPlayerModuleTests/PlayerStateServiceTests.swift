421615bdb2d02072476c239cdbb0561d4a0a02e3
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-11-22 11:54:59 +0200

    Observer receives previous state if subject has it before subscription

--- PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/PlayerStateServiceTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayerModuleTests/PlayerStateServiceTests.swift
@@ -3,26 +3,66 @@
 import XCTest
 import PodcastsModule
 
-struct CurrentPlayerState {
-    let data: Int
+enum CurrentPlayerState {
+    case noPlayingItem
 }
 
 protocol AudioPlayerObserver {
     func update(with playerState: CurrentPlayerState)
 }
 
+protocol AudioPlayerStateSubject {
+    func attach(observer: AudioPlayerObserver)
+}
+
 class PlayerStateServiceTests: XCTestCase {
     
-    func test_init_doesNotDeliversValueOnCreation() {
+    func test_init_doesNotDeliversValueOnCreationWithoutSubscription() {
         var receivedData: CurrentPlayerState?
-        _ = ConcreteAudioPlayerObserver(onReceiveUpdates: { state in
+        _ = AudioPlayerStateObserver(onReceiveUpdates: { state in
             receivedData = state
         })
         
         XCTAssertNil(receivedData)
     }
     
-    private class ConcreteAudioPlayerObserver: AudioPlayerObserver {
+    func test_onReceiveValues_deliversStateAfterSubscription() {
+        let playerSubject = AudioPlayerStateSubjectSpy()
+        var receivedPlayerState: CurrentPlayerState?
+        let exp = expectation(description: "Wait on receive state")
+        let observer = AudioPlayerStateObserver(onReceiveUpdates: { state in
+            receivedPlayerState = state
+            exp.fulfill()
+        })
+        
+        playerSubject.attach(observer: observer)
+        playerSubject.receiveNewPlayerState(.noPlayingItem)
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedPlayerState, .noPlayingItem)
+    }
+    
+    func test_onReceiveValues_deliversPreviousStateAfterSubscription() {
+        let playerSubject = AudioPlayerStateSubjectSpy()
+        var receivedPlayerState: CurrentPlayerState?
+        let exp = expectation(description: "Wait on receive state")
+        let observer = AudioPlayerStateObserver(onReceiveUpdates: { state in
+            receivedPlayerState = state
+            exp.fulfill()
+        })
+        
+        playerSubject.receiveNewPlayerState(.noPlayingItem)
+        playerSubject.attach(observer: observer)
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedPlayerState, .noPlayingItem)
+    }
+    
+    // MARK: - Helpers
+    
+    private class AudioPlayerStateObserver: AudioPlayerObserver {
         var onReceiveUpdates: (CurrentPlayerState) -> Void
         
         init(onReceiveUpdates: @escaping (CurrentPlayerState) -> Void) {
@@ -30,7 +70,34 @@ class PlayerStateServiceTests: XCTestCase {
         }
         
         func update(with playerState: CurrentPlayerState) {
-            
+            onReceiveUpdates(playerState)
+        }
+    }
+    
+    private class AudioPlayerStateSubjectSpy: AudioPlayerStateSubject {
+        
+        private var previosState: CurrentPlayerState?
+        private var observer: AudioPlayerObserver?
+        
+        func attach(observer: AudioPlayerObserver) {
+            self.observer = observer
+            updateStateOfAttachedObserverIfPreviousStateExists(observer)
+        }
+        
+        func receiveNewPlayerState(_ state: CurrentPlayerState) {
+            previosState = state
+            updateObserver(with: state)
+        }
+        
+        private func updateObserver(with state: CurrentPlayerState) {
+            observer?.update(with: state)
+        }
+        
+        private func updateStateOfAttachedObserverIfPreviousStateExists(_ observer: AudioPlayerObserver) {
+            guard let previosState = previosState else {
+                return
+            }
+            updateObserver(with: previosState)
         }
     }
 }

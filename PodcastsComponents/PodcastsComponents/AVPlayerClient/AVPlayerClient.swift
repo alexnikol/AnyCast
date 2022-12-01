0f9f1713a3e204f74855ed2997aa3d990c49bb51
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-01 15:16:31 +0200

    Updated list of available platforms for each module

---
+++ PodcastsComponents/PodcastsComponents/AVPlayerClient/AVPlayerClient.swift
@@ -0,0 +1,5 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import Foundation
+
+class AVPlayerClient {}
---
+++ PodcastsComponents/PodcastsComponents/AVPlayerClientTests/AVPlayerClientTests.swift
@@ -0,0 +1,6 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import XCTest
+@testable import AVPlayerClient
+
+final class AVPlayerClientTests: XCTestCase {}

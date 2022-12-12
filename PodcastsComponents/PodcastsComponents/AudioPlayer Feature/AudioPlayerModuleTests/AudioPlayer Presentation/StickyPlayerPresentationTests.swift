e5fc38e82604177c380e60987bc15c7c7c0ae3c3
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-12 11:29:03 +0200

    `StickyPlayerPresenter` send update message to resource view on did receive playing item

--- PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/AudioPlayer Presentation/StickyPlayerPresentationTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/AudioPlayer Presentation/StickyPlayerPresentationTests.swift
@@ -4,11 +4,27 @@ import XCTest
 import AudioPlayerModule
 import PodcastsModule
 
-protocol StickyAudioPlayerView {}
+struct StickyAudioPlayerViewModel {
+    
+}
+
+protocol StickyAudioPlayerView {
+    func display(viewModel: StickyAudioPlayerViewModel)
+}
 
 class StickyPlayerPresenter {
+    private let resourceView: StickyAudioPlayerView
+    
     init(resourceView: StickyAudioPlayerView) {
-        
+        self.resourceView = resourceView
+    }
+    
+    func map(playingItem: PlayingItem) -> StickyAudioPlayerViewModel {
+        StickyAudioPlayerViewModel()
+    }
+    
+    func didReceivePlayerState(with playingItem: PlayingItem) {
+        resourceView.display(viewModel: map(playingItem: playingItem))
     }
 }
 
@@ -18,6 +34,15 @@ class StickyPlayerPresentationTests: XCTestCase {
         let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    func test_didReceiveNewPlayerState_displaysNewPlayerState() {
+        let (sut, view) = makeSUT()
+        
+        let playingItem = makePlayingItem(playbackState: .pause, currentTimeInSeconds: 0, totalTime: .notDefined, playbackSpeed: .x0_75)
+        sut.didReceivePlayerState(with: playingItem)
+        
+        XCTAssertEqual(view.messages, [.update])
     }
     
     // MARK: - Helpers
@@ -31,7 +56,37 @@ class StickyPlayerPresentationTests: XCTestCase {
     }
     
     private class ViewSpy: StickyAudioPlayerView {
-        enum Message {}
+        enum Message {
+            case update
+        }
         private(set) var messages: [Message] = []
+        
+        func display(viewModel: StickyAudioPlayerViewModel) {
+            messages.append(.update)
+        }
+    }
+    
+    private func makePlayingItem(
+        playbackState: PlayingItem.PlaybackState,
+        currentTimeInSeconds: Int,
+        totalTime: EpisodeDuration,
+        playbackSpeed: PlaybackSpeed
+    ) -> PlayingItem {
+        PlayingItem(
+            episode: makeUniqueEpisode(),
+            podcast: makePodcast(),
+            updates: [
+                .playback(playbackState),
+                .progress(
+                    .init(
+                        currentTimeInSeconds: currentTimeInSeconds,
+                        totalTime: totalTime,
+                        progressTimePercentage: 0.1234
+                    )
+                ),
+                .volumeLevel(0.5),
+                .speed(playbackSpeed)
+            ]
+        )
     }
 }

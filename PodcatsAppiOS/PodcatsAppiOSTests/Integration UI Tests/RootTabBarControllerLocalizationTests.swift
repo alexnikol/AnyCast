8e62cac8cbbcce95269a561dacca6aa8b7b84f22
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-13 11:45:33 +0200

    Removed Base localization and added en and ua versions

--- PodcatsAppiOS/PodcatsAppiOSTests/Integration UI Tests/RootTabBarControllerLocalizationTests.swift
+++ PodcatsAppiOS/PodcatsAppiOSTests/Integration UI Tests/RootTabBarControllerLocalizationTests.swift
@@ -3,12 +3,12 @@
 import XCTest
 @testable import Podcats
 
-//class RootTabBarControllerLocalizationTests: XCTestCase, LocalizationTestCase {
-//    
-//    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
-//        let table = "Main"
-//        let presentationBundle = Bundle(for: RootTabBarControllerPresenter.self)
-//
-//        checkForMissingLocalizationInAllSupportedLanguages(bundle: presentationBundle, table: table)
-//    }
-//}
+class RootTabBarControllerLocalizationTests: XCTestCase, LocalizationTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "Main"
+        let presentationBundle = Bundle(for: RootTabBarControllerPresenter.self)
+
+        checkForMissingLocalizationInAllSupportedLanguages(bundle: presentationBundle, table: table)
+    }
+}

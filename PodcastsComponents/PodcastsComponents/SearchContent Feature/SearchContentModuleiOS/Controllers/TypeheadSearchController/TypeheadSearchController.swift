a70b07265ee80cb6d830dff5a824905730ab1a03
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-19 14:51:05 +0200

    `SearchContentModuleiOS` added

---
+++ PodcastsComponents/PodcastsComponents/SearchContent Feature/SearchContentModuleiOS/Controllers/TypeheadSearchController/TypeheadSearchController.swift
@@ -0,0 +1,10 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import UIKit
+
+final class TypeheadSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

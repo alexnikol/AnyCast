12ed4dcf5f23aa96f471c16d866d69fe8ee4805f
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-23 14:28:21 +0200

    Typehead search endpoint tested

---
+++ PodcastsComponents/PodcastsComponents/SearchContent Feature/SearchContentModuleTests/TypeheadSearchContent/TypeheadSearchContent API/TypeheadSearchEndpointTests.swift
@@ -0,0 +1,16 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import XCTest
+import SearchContentModule
+
+final class TypeheadSearchEndpointTests: XCTestCase {
+    
+    func test_generalSearch_endpointURL() {
+        let baseURL = URL(string: "https://listen-api-test.listennotes.com")!
+        let searchTerm = "any term"
+        let received = SearchEndpoint.getTypeheadSearch(term: searchTerm).url(baseURL: baseURL)
+        let exptected = URL(string: "https://listen-api-test.listennotes.com/api/v2/typeahead?q=any%20term")
+
+        XCTAssertEqual(received, exptected)
+    }
+}

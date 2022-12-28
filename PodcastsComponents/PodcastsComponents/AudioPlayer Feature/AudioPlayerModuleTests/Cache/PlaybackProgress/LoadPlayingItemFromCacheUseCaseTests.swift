59d779d8f474e0f40dedf6118d59297d8dce7dc2
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-28 16:58:21 +0200

    Move shared `PlaybackProgressStoreSpy` to shared folder

---
+++ PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/LoadPlayingItemFromCacheUseCaseTests.swift
@@ -0,0 +1,22 @@
+// Copyright © 2022 Almost Engineer. All rights reserved.
+
+import XCTest
+import SharedTestHelpersLibrary
+import AudioPlayerModule
+
+final class LoadPlayingItemFromCacheUseCaseTests: XCTestCase {
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: LocalPlaybackProgressLoader, store: PlaybackProgressStoreSpy) {
+        let store = PlaybackProgressStoreSpy()
+        let sut = LocalPlaybackProgressLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}

43c58aa6ba7b83af1b75b829f05b7e38d746e31e
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-28 14:22:05 +0200

    `LocalPlaybackProgressLoader` save command sends delete cache message

--- PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/CachePlaybackProgressUseCaseTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/CachePlaybackProgressUseCaseTests.swift
@@ -4,17 +4,27 @@ import XCTest
 import SharedTestHelpersLibrary
 import AudioPlayerModule
 
-class LocalPlayingItem {
-    
-}
 
 final class LocalPlaybackProgressLoader {
+    typealias SaveResult = Error?
     
-    init(store: PlaybackProgressStore, currentDate: @escaping () -> Date) {}
+    private let store: PlaybackProgressStore
+    private let currentDate: () -> Date
+    
+    init(store: PlaybackProgressStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    func save(_ playingItem: AudioPlayerModule.PlayingItem, completion: @escaping (SaveResult) -> Void) {
+        store.deleteCachedPlayingItem(completion: { _ in })
+    }
 }
 
 protocol PlaybackProgressStore {
+    typealias DeletionCompletion = (Error?) -> Void
     
+    func deleteCachedPlayingItem(completion: @escaping DeletionCompletion)
 }
 
 class PlaybackProgressStoreSpy: PlaybackProgressStore {
@@ -23,6 +33,10 @@ class PlaybackProgressStoreSpy: PlaybackProgressStore {
     }
     
     private(set) var receivedMessages: [Message] = []
+    
+    func deleteCachedPlayingItem(completion: @escaping DeletionCompletion) {
+        receivedMessages.append(.deleteCache)
+    }
 }
 
 final class CachePlaybackProgressUseCaseTests: XCTestCase {
@@ -31,6 +45,15 @@ final class CachePlaybackProgressUseCaseTests: XCTestCase {
         let (_, store) = makeSUT()
         
         XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let (sut, store) = makeSUT()
+        let playingItem = makePlayingItemModels()
+        
+        sut.save(playingItem.model) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCache])
     }
     
     // MARK: - Helpers
@@ -47,10 +70,12 @@ final class CachePlaybackProgressUseCaseTests: XCTestCase {
         return (sut, store)
     }
     
-    private func makePlayingItem() -> PlayingItem {
-        PlayingItem(
-            episode: makeUniqueEpisode(),
-            podcast: makePodcast(),
+    private func makePlayingItemModels() -> (model: PlayingItem, localModel: LocalPlayingItem) {
+        let episode = makeUniqueEpisode()
+        let podcast = makePodcast()
+        let model = PlayingItem(
+            episode: episode,
+            podcast: podcast,
             updates: [
                 .playback(.playing),
                 .progress(
@@ -63,5 +88,33 @@ final class CachePlaybackProgressUseCaseTests: XCTestCase {
                 .volumeLevel(0.5)
             ]
         )
+        
+        let localEpisode = LocalPlayingEpisode(
+            id: episode.id,
+            title: episode.title,
+            thumbnail: episode.thumbnail,
+            audio: episode.audio,
+            publishDateInMiliseconds: episode.publishDateInMiliseconds
+        )
+        
+        let localPodcast = LocalPlayingPodcast(id: podcast.id, title: podcast.title, publisher: podcast.publisher)
+        
+        let localModel = LocalPlayingItem(
+            episode: localEpisode,
+            podcast: localPodcast,
+            updates: [
+                .playback(.playing),
+                .progress(
+                    .init(
+                        currentTimeInSeconds: 10,
+                        totalTime: .notDefined,
+                        progressTimePercentage: 0.1
+                    )
+                ),
+                .volumeLevel(0.5)
+            ]
+        )
+        
+        return (model, localModel)
     }
 }

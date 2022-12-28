659aeb93fa71768e33e1b1419c0eb690cc6c4b0d
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-28 17:30:52 +0200

    `LocalPlaybackProgressLoader` load has not side effect on retrieve

--- PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/LoadPlayingItemFromCacheUseCaseTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/LoadPlayingItemFromCacheUseCaseTests.swift
@@ -48,6 +48,16 @@ final class LoadPlayingItemFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_hasNoSideEffectsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: retrievalError)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(

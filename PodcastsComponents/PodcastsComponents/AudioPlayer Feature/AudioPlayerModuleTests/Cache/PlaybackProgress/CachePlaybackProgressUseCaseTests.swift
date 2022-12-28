5ae4c4461e3e4d9cf4a974ea875e83ecdd42e395
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2022-12-28 12:35:58 +0200

    `LocalPlaybackProgressLoader` does not send messages upon creation

--- PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/CachePlaybackProgressUseCaseTests.swift
+++ PodcastsComponents/PodcastsComponents/AudioPlayer Feature/AudioPlayerModuleTests/Cache/PlaybackProgress/CachePlaybackProgressUseCaseTests.swift
@@ -3,5 +3,36 @@
 import XCTest
 import SharedTestHelpersLibrary
 
+final class LocalPlaybackProgressLoader {
+    
+    init(store: PlaybackProgressStore, currentDate: @escaping () -> Date) {}
+}
+
+protocol PlaybackProgressStore {
+    
+}
+
+class PlaybackProgressStoreSpy: PlaybackProgressStore {
+    private(set) var receivedMessages: [Int] = []
+}
+
 final class CachePlaybackProgressUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: LocalPlaybackProgressLoader, store: PlaybackProgressStoreSpy) {
+        let store = PlaybackProgressStoreSpy()
+        let sut = LocalPlaybackProgressLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
 }

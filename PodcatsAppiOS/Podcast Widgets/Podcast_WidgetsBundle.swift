165f0dacef8cfb2b7c62b1db73d8d0eea0827ac1
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-01-11 14:43:56 +0200

    UI widget setup

---
+++ PodcatsAppiOS/Podcast Widgets/Podcast_WidgetsBundle.swift
@@ -0,0 +1,12 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Podcast_WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Podcast_Widgets()
+        Podcast_WidgetsLiveActivity()
+    }
+}

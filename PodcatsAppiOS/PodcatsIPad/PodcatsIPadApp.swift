58559cbf399e013eff2cb5ecc6265975f583cfbf
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-01-18 11:50:47 +0200

    `PodcatsIPad` Application added for specific SwiftUI iPad application

---
+++ PodcatsAppiOS/PodcatsIPad/PodcatsIPadApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import SwiftUI
+
+@main
+struct PodcatsIPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
